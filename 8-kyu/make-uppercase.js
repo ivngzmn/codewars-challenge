69b1ed7e26650875a5ced60b8f15501965164591
Ivan Guzman <hello@ivanguzman.dev>
2022-03-14 18:09:46 -0700

    make upper case 8 kyu

---
+++ 8-kyu/make-uppercase.js
@@ -0,0 +1,18 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+Kata title: MakeUpperCase
+Write a function which converts the input string to uppercase.
+*/
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
+
+// es6 solution
+// const makeUpperCase = (str) => str.toUpperCase();
