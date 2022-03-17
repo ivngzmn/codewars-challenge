7563b65d70c677f02e9dfb9c22a4adb5b2fb9bba
Ivan Guzman <hello@ivanguzman.dev>
2022-03-17 01:16:28 -0700

    convert a bool to a str

---
+++ 8-kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,26 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+Kata title: Convert a Boolean to a String!
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+function booleanToString(b) {
+  // your code here
+  new String(b);
+  return String(b);
+}
+
+console.log(booleanToString(true)); // Expected result "true"
+console.log(booleanToString(false)); // Expected result "false"
+console.log(booleanToString("false")); // Expected result "false"
+console.log(typeof String("true"));
