0e97a1b1de260534decb481f5c5cc5f73a8f3860
Ivan Guzman <hello@ivanguzman.dev>
2022-03-18 01:18:18 -0700

    return negative

---
+++ 8-kyu/return-negative.js
@@ -0,0 +1,35 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/55685cd7ad70877c23000102
+Kata title: Return Negative
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+Examples
+
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+
+Notes
+
+    The number can be negative already, in which case no change is required.
+    Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+
+*/
+
+function makeNegative(num) {
+    return num > 0 ? -Math.abs(num) : num;
+}
+
+console.log(makeNegative(42)); // Expected output -42
+console.log(makeNegative(1)); // Expected output -1
+console.log(makeNegative(-5)); // Expected output -5
+console.log(makeNegative(0)); // Expected output 0
+console.log(makeNegative(0.12)); // Expected output -0.12
