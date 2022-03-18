5fc0aaede931251b331bde3f7a44a5a8da6571b7
Ivan Guzman <hello@ivanguzman.dev>
2022-03-17 22:53:40 -0700

    find the capitals

---
+++ 7-kyu/find-the-capitals.js
@@ -0,0 +1,34 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/539ee3b6757843632d00026b/train/javascript
+Kata title: Find the capitals
+
+Instructions
+
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+Example
+
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+*/
+
+const capitals = function (word) {
+    let l = word.split('');
+    console.log(l);
+    let upperCaseIndex = [];
+
+    for (let i = 0; i < word.length; i++) {
+        if (l[i] === l[i].toUpperCase()) {
+            upperCaseIndex.push(i);
+        }
+    }
+
+    return upperCaseIndex;
+};
+
+console.log(capitals('CodEWaRs')); // Expected result is [0,3,4,6]
