adb762f94e9ce605dd3a960552abfede6f638f15
Ivan Guzman <hello@ivanguzman.dev>
2022-03-18 00:11:47 -0700

    sum of positive

---
+++ 8-kyu/sum-of-positive.js
@@ -0,0 +1,41 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+Kata title: Sum of positive
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+*/
+
+function positiveSum(arr) {
+    return arr.reduce(
+        (previousVal, currentVal) =>
+            previousVal + (currentVal > 0 ? currentVal : 0),
+        0
+    );
+}
+
+// function positiveSum(arr) {
+//   let total = 0;
+//   for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+//     if (arr[i] > 0) {                   // if arr[i] is greater than zero
+//       total += arr[i];                  // add arr[i] to total
+//     }
+//   }
+//   return total;                         // return total
+// }
+
+console.log(positiveSum([1, 2, 3, 4, 5])); // Expected result 15
+console.log(positiveSum([1, -2, 3, 4, 5])); // Expected result 13
+console.log(positiveSum([])); // Expected result 0
+console.log(positiveSum([-1, -2, -3, -4, -5])); // Expected result 0
+console.log(positiveSum([-1, 2, 3, 4, -5])); // Expected result 9
