ec0da162e63f75712bb9e2c963ed750dd2131ad4
Ivan Guzman <hello@ivanguzman.dev>
2022-03-22 23:06:01 -0700

    opposite number

---
+++ 8-kyu/opposite-number.js
@@ -0,0 +1,29 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+Kata title: Opposite number
+
+Instructions
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+    return -number;
+}
+
+console.log(opposite(1)); // Expected result -1
+console.log(opposite(14)); // Expected result -14
+console.log(opposite(-34)); // Expected result 34
+console.log(opposite(-420)); // Expected result 420
