769b69fabbdfaccd179addd720011b30d5d4c6e3
Ivan Guzman <hello@ivanguzman.dev>
2022-03-24 22:41:44 -0700

    reverse strings

---
+++ 8-kyu/reverse-strings.js
@@ -0,0 +1,25 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+Kata title: Reversed Strings    
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+function solution(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(solution('world')); // 'dlrow'
+console.log(solution('hello')); // 'olleh'
+console.log(solution('')); // ''
+console.log(solution('h')); // 'h'
