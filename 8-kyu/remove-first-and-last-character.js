145c984278c5a03df93c090b3a6183c33a595324
Ivan Guzman <hello@ivanguzman.dev>
2022-03-24 22:33:21 -0700

    remove first and last character

---
+++ 8-kyu/remove-first-and-last-character.js
@@ -0,0 +1,21 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+Kata title: Remove First and Last Character
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+*/
+
+function removeChar(str) {
+    return str.slice(1, -1);
+}
+
+console.log(removeChar('eloquent')); //'loquen'
+console.log(removeChar('country')); //'ountr'
+console.log(removeChar('person')); //'erson'
+console.log(removeChar('ooopsss')); //'oopss'
