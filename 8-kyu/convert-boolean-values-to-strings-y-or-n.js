29923e46063630d8da02ebe0646e6198044a680d
Ivan Guzman <hello@ivanguzman.dev>
2022-03-25 23:48:44 -0700

    Convert boolean values to strings 'Yes' or 'No'

---
+++ 8-kyu/convert-boolean-values-to-strings-y-or-n.js
@@ -0,0 +1,18 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+Kata title: Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+}
+
+console.log(boolToWord(true)); // Expected result 'Yes'
+console.log(boolToWord(false)); // Expected result 'No'
