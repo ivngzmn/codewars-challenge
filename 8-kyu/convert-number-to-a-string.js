33ba7c02c7ff7e59cdd5407d3b4459a4f5ff1934
Ivan Guzman <hello@ivanguzman.dev>
2022-03-25 23:37:44 -0700

    convert a number to a string

---
+++ 8-kyu/convert-number-to-a-string.js
@@ -0,0 +1,24 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+Kata title: Convert a Number to a String!
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+Examples:
+
+123 --> "123"
+999 --> "999"
+*/
+
+function numberToString(num) {
+    return num.toString();
+}
+
+console.log(numberToString(67)); //Expected result '67'
