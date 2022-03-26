a4ace7dd1cdcd0968dc88ef0b53f571a0da51853
Ivan Guzman <hello@ivanguzman.dev>
2022-03-26 00:03:41 -0700

    string repeat

---
+++ 8-kyu/string-repeat.js
@@ -0,0 +1,27 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+Kata title: String repeat
+
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+
+*/
+
+function repeatStr(n, s) {
+    return s.repeat(n);
+}
+
+console.log(repeatStr(3, '*')); // Expected result '***'
+console.log(repeatStr(5, '#')); // Expected result '#####'
+console.log(repeatStr(3, 'ha ')); // Expected result 'ha ha '
+console.log(repeatStr(6, 'I')); // Expected result 'IIIIII'
+console.log(repeatStr(5, 'Hello')); // Expected result 'HelloHelloHelloHelloHello'
