8cec6ac82c8edcbe4bd0a819d0bfbb494f9721eb
Ivan Guzman <hello@ivanguzman.dev>
2022-03-29 22:36:22 -0700

    grasshopper sumation

---
+++ 8-kyu/grasshopper-summation.js
@@ -0,0 +1,29 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+Kata title: Grasshopper - Summation
+
+Summation
+
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+*/
+
+const summation = (num) => (num * (num + 1)) / 2;
+
+console.log(summation(1)); // Expected result 1
+console.log(summation(2)); // Expected result 3
+console.log(summation(8)); // Expected result 36
