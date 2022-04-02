65ab8392112219a4e4ac9bcf47cdad971635a563
Ivan Guzman <hello@ivanguzman.dev>
2022-04-01 23:52:13 -0700

    returning strings

---
+++ 8-kyu/returning-strings.js
@@ -0,0 +1,23 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/55a70521798b14d4750000a4
+Kata title: Returning Strings
+
+Description:
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+*/
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet('Ryan')); // expected result "Hello, Ryan how are you doing today?"
+console.log(greet('Shingles')); // expected result "Hello, Shingles how are you doing today?"
