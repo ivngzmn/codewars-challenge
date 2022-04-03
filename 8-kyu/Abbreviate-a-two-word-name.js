bc2b7498b0179753b147dcbc8fb8120327fb7833
Ivan Guzman <hello@ivanguzman.dev>
2022-04-02 23:34:09 -0700

    Abbreviate a 2 word name

---
+++ 8-kyu/Abbreviate-a-two-word-name.js
@@ -0,0 +1,30 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+Kata title: Abbreviate a Two Word Name
+
+Instructions
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+function abbrevName(name) {
+    return name
+        .split(' ')
+        .map((firstLastIn) => firstLastIn.slice(0, 1).toUpperCase()) // substr is deprecated
+        .join('.');
+}
+
+console.log(abbrevName('Sam Harris'));
+console.log(abbrevName('patrick feeney'));
