e542c332c4f21ec622c5b52c34546288919710c5
Ivan Guzman <hello@ivanguzman.dev>
2022-04-04 01:49:15 -0700

    jenny's secret message

---
+++ 8-kyu/jenny's-secret-message.js
@@ -0,0 +1,29 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/55225023e1be1ec8bc000390
+Kata title: Jenny's secret message
+
+Instructions
+Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+Can you help her?
+*/
+function greet(name) {
+    return `Hello, ${name === 'Johnny' ? 'my love' : name}!`; // as DRY as possible
+}
+
+console.log(greet('Johnny'));
+console.log(greet('Bob'));
+
+// function greet(name) {
+//     if (name === 'Johnny') {
+//         return 'Hello, my love!';
+//     } else {
+//         return 'Hello, ' + name + '!';
+//     }
+// }
