8393e8262cb48451cdade029e8304678d4ed0bb9
Ivan Guzman <hello@ivanguzman.dev>
2022-04-15 01:00:22 -0700

    desending order

---
+++ 7-kyu/descending-order.js
@@ -0,0 +1,33 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+Kata title: Descending Order
+
+Instructions
+
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+Examples:
+
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+*/
+
+function descendingOrder(nums) {
+    return +(nums + '').split('').sort().reverse().join('');
+}
+
+console.log(descendingOrder(42145), 54421);
+console.log(descendingOrder(145263), 654321);
+console.log(descendingOrder(123456789), 987654321);
+console.log(descendingOrder(15), 51);
+console.log(descendingOrder(1021), 2110);
+console.log(descendingOrder(645641987465164), 987666655444411);
