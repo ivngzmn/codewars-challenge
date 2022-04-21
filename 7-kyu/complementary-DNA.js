e2d36eefefde22fbb6ad7880ba0015aa99af21b8
Ivan Guzman <hello@ivanguzman.dev>
2022-04-20 23:58:37 -0700

    complementary dna

---
+++ 7-kyu/complementary-DNA.js
@@ -0,0 +1,51 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+Kata title: Complementary DNA
+
+Instructions
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+
+dnaStrand []        `shouldBe` []
+dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+*/
+
+function DNAStrand(dna) {
+    return dna
+        .split('')
+        .map((char) =>
+            char === 'A'
+                ? 'T'
+                : char === 'T'
+                ? 'A'
+                : char === 'C'
+                ? 'G'
+                : char === 'G'
+                ? 'C'
+                : ''
+        )
+        .join('');
+}
+
+console.log(DNAStrand(''), '');
+console.log(DNAStrand('AAAA'), 'TTTT');
+console.log(DNAStrand('ATTGC'), 'TAACG');
+console.log(DNAStrand('GTAT'), 'CATA');
