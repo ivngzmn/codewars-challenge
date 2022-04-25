2ca07ad7923728fcbf7cc1ec32a22bf9d9532446
Ivan Guzman <hello@ivanguzman.dev>
2022-04-24 22:59:23 -0700

    sum of two lowest positive integers

---
+++ 7-kyu/sum-of-two-lowest-positive-integers.js
@@ -0,0 +1,23 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/558fc85d8fd1938afb000014
+Kata title: Sum of two lowest positive integers
+
+Instructions
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+function sumTwoSmallestNumbers(nums) {
+    let a = nums.sort((a, b) => a - b);
+    return a[0] + a[1];
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
