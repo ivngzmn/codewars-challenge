2f8f0484726a41f050a6b42ecf6522c1533034e9
Ivan Guzman <hello@ivanguzman.dev>
2022-04-26 15:18:22 -0700

    array combinations

---
+++ 6-kyu/array-combinations.js
@@ -0,0 +1,34 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/59e66e48fc3c499ec5000103
+Kata title: Array combinations
+
+Instructions
+In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilities. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+See test cases for more examples.
+
+Good luck!
+*/
+
+// function solve(arr) {
+//     return arr.reduce((res, curr) => (res *= new Set(curr).size), 1);
+// }
+
+// alternate solution with map, Set and reduce
+function solve(arr) {
+    return arr
+        .map((el) => new Set(el).size)
+        .reduce((acc, curr) => acc * curr, 1);
+}
+
+console.log(solve([[1, 2], [4], [5, 6]]));
