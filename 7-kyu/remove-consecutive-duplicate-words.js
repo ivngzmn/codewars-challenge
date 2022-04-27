4fe85f1595daf5db800a1e97486e4d8473f18e26
Ivan Guzman <hello@ivanguzman.dev>
2022-04-27 11:43:40 -0700

    remove consecutive duplicate words

---
+++ 7-kyu/remove-consecutive-duplicate-words.js
@@ -0,0 +1,30 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5b39e91ee7a2c103300018b3
+Kata title: Remove consecutive duplicate words
+
+Instructions
+Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+"alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+--> "alpha beta gamma delta alpha beta gamma delta"
+*/
+
+const removeConsecutiveDuplicates = (string) =>
+    string
+        .split(' ')
+        .filter((word, i, arr) => word !== arr[i + 1])
+        .join(' ');
+
+console.log(
+    removeConsecutiveDuplicates(
+        'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+    ),
+    'alpha beta gamma delta alpha beta gamma delta'
+);
