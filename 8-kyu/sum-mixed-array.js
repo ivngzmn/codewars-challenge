941c9f82472483a2b7ac87fb922f45a4ab48201e
Ivan Guzman <hello@ivanguzman.dev>
2022-04-27 00:45:38 -0700

    sum mixed array

---
+++ 8-kyu/sum-mixed-array.js
@@ -0,0 +1,23 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57eaeb9578748ff92a000009
+Kata title: Sum Mixed Array
+
+Instructions
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(arr) {
+    return arr.reduce((acc, curr) => acc + Number(curr), 0);
+}
+
+console.log(sumMix([9, 3, '7', '3']), 22);
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 41);
