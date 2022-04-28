452c6934079100437b876a6b8965041a8e893130
Ivan Guzman <hello@ivanguzman.dev>
2022-04-27 23:24:37 -0700

    duplicate encoder

---
+++ 6-kyu/duplicate-encoder.js
@@ -0,0 +1,38 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+Kata title: Duplicate Encoder
+
+Instructions
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+Examples
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+
+Notes
+
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+*/
+
+function duplicateEncode(word) {
+    return word
+        .toLowerCase() // ignore case
+        .split('') // split word to individual letters in array
+        .map((val, i, arr) => {
+            return arr.indexOf(val) === arr.lastIndexOf(val) ? '(' : ')';
+        }) // encode individual letters to either '(' or ')' depending on how often they occur in array
+        .join(''); // join encoded symbols into a string
+}
+
+console.log(duplicateEncode('din'), '(((');
+console.log(duplicateEncode('recede'), '()()()');
+console.log(duplicateEncode('Success'), ')())())', 'should ignore case');
+console.log(duplicateEncode('(( @'), '))((');
