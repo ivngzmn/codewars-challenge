c6222a3a01a39699f3ca655386690f08f96f56ad
Ivan Guzman <hello@ivanguzman.dev>
2022-04-27 21:56:19 -0700

    count characters in your string

---
+++ 6-kyu/count-characters-in-your-string.js
@@ -0,0 +1,25 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/52efefcbcdf57161d4000091
+Kata title: Count characters in your string
+
+Instructions
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+*/
+
+function count(string) {
+    let hash = {};
+    string.split('').map((letter) => (hash[letter] = hash[letter] + 1 || 1));
+    return hash;
+}
+
+console.log(count('aba'), { a: 2, b: 1 });
+console.log(count('abccbbaaabc'), { a: 4, b: 4, c: 3 });
+console.log(count(''), {});
