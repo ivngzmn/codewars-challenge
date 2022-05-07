7de453c2ca14e8e0de5930423ffbd8f2a257765d
Ivan Guzman <hello@ivanguzman.dev>
2022-05-07 15:31:37 -0700

    calculate average

--- 8-kyu/calculate-average.js
+++ 8-kyu/calculate-average.js
@@ -16,5 +16,16 @@ Note: Empty arrays should return 0.
 */
 
 function find_average(arr) {
-    return;
+    return arr.length > 0
+        ? arr.reduce((acc, curr) => acc + curr, 0) / arr.length
+        : 0;
 }
+
+console.log(find_average([1, 1, 1]), 1);
+console.log(find_average([]), 0);
+console.log(find_average([1, 2, 3]), 2);
+console.log(find_average([1, 2, 3, 4]), 2.5);
+console.log(
+    find_average([67, 12, 58, 61, 19, 71, 100, 25, 20, 80, 64, 76, 46]),
+    53.76923076923077
+);
