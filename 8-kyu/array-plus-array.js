5a0dce7b2dcda85e5a3656d9c8a0e579795ffcdc
Ivan Guzman <hello@ivanguzman.dev>
2022-05-11 16:14:59 -0700

    array plus array

---
+++ 8-kyu/array-plus-array.js
@@ -0,0 +1,26 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+Kata title: Array plus array
+
+Description:
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+function arrayPlusArray(arr1, arr2) {
+    return (
+        arr1.reduce((acc, curr) => acc + curr, 0) +
+        arr2.reduce((acc, curr) => acc + curr, 0)
+    );
+}
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
