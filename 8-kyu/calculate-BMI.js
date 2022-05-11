5b2c7547452d1a98a381329d2a33f588e7d240d4
Ivan Guzman <hello@ivanguzman.dev>
2022-05-10 18:19:10 -0700

    calculate BMI

---
+++ 8-kyu/calculate-BMI.js
@@ -0,0 +1,34 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝ ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57a429e253ba3381850000fb
+Kata title: Calculate BMI
+
+Description:
+Write function bmi that calculates body mass index (bmi = weight / height^2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+function bmi(weight, height) {
+    let calcBMI = weight / height ** 2;
+    return calcBMI <= 18.5
+        ? 'Underweight'
+        : calcBMI <= 25.0
+        ? 'Normal'
+        : calcBMI <= 30.0
+        ? 'Overweight'
+        : 'Obese';
+}
+
+console.log(bmi(80, 1.8), 'Normal');
