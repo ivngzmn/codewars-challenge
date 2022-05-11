50bc27c9da2ec69d07e5eb1781eafad53f6b2f7e
Ivan Guzman <hello@ivanguzman.dev>
2022-05-11 16:28:34 -0700

    sentence smash

---
+++ 8-kyu/sentence-smash.js
@@ -0,0 +1,30 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/53dc23c68a0c93699800041d
+Kata title: Sentence Smash
+
+Description:
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+Example
+
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+*/
+
+function smash(words) {
+    return words.join(' ');
+}
+
+console.log(smash([]), '');
+console.log(smash(['hello']), 'hello');
+console.log(smash(['hello', 'world']), 'hello world');
+console.log(smash(['hello', 'amazing', 'world']), 'hello amazing world');
+console.log(
+    smash(['this', 'is', 'a', 'really', 'long', 'sentence']),
+    'this is a really long sentence'
+);
