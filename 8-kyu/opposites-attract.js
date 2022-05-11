b888f7fe3203371591fd03d9dcc9955e616a7e3a
Ivan Guzman <hello@ivanguzman.dev>
2022-05-10 19:29:20 -0700

    opposites attract

---
+++ 8-kyu/opposites-attract.js
@@ -0,0 +1,29 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/555086d53eac039a2a000083
+Kata title: Opposite Attract
+
+Instructions
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+*/
+
+function loveFunc(flower1, flower2) {
+    return flower1 % 2 === 0 && flower2 % 2 !== 0
+        ? true
+        : flower1 % 2 !== 0 && flower2 % 2 === 0
+        ? true
+        : false;
+}
+
+console.log(loveFunc(1, 4), true);
+console.log(loveFunc(2, 2), false);
+console.log(loveFunc(0, 1), true);
+console.log(loveFunc(0, 0), false);
