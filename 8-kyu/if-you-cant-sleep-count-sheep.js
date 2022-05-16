e0b84d6aa7e60203a3596f4cab93b882fde677fd
Ivan Guzman <hello@ivanguzman.dev>
2022-05-15 22:55:33 -0700

    If you can't sleep, just count sheep!!

---
+++ 8-kyu/if-you-cant-sleep-count-sheep.js
@@ -0,0 +1,29 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+Kata title: If you can't sleep, just count sheep!!
+
+Description:
+If you can't sleep, just count sheep!!
+Task:
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+
+const countSheep = function (num) {
+    let sheepCount = '';
+    for (let sheep = 1; sheep <= num; sheep++) {
+        sheepCount += `${sheep} sheep...`;
+    }
+    return sheepCount;
+};
+
+console.log(countSheep(0), '');
+console.log(countSheep(1), '1 sheep...');
+console.log(countSheep(2), '1 sheep...2 sheep...');
+console.log(countSheep(3), '1 sheep...2 sheep...3 sheep...');
