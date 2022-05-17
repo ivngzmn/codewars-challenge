2dd6d797c43e1d366c1c4588650904655670a251
Ivan Guzman <hello@ivanguzman.dev>
2022-05-16 19:49:00 -0700

    area or perimeter

---
+++ 8-kyu/area-or-perimeter.js
@@ -0,0 +1,33 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+Kata title: Area or Perimeter
+
+Instructions
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+area_or_perimeter(6, 10) --> 32
+area_or_perimeter(3, 3) --> 9
+
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+//PREP
+// whole num? Y, negative num? N, strings? N
+// given length and width if num1 and num2 are equal multiply those two nums
+// else we will return the perimeter console.log? Y
+
+function areaOrPerimeter(length, width) {
+    // check if length == width
+    return length === width ? length * width : (length + width) * 2;
+    // if equal multiply length and width
+    // else (length + width) * 2
+}
+
+console.log(areaOrPerimeter(6, 10)); // != return 32
+console.log(areaOrPerimeter(3, 3)); // == return 9
