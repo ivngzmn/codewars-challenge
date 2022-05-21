9e5a230bb0a5d55d3b703df39dac89873895dfd0
Ivan Guzman <hello@ivanguzman.dev>
2022-05-20 23:30:43 -0700

    Quarter of the year

---
+++ 8-kyu/JS/quarter-of-the-year.js
@@ -0,0 +1,23 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+Kata title: Quarter of the year
+
+Instructions
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+*/
+//PREP
+//
+
+const quarterOf = (month) => {};
+
+console.log(quarterOf(3)); // 1st quarter
+console.log(quarterOf(8)); // 3rd quarter
+console.log(quarterOf(11)); // 4th quarter
