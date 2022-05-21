390f06feaf22486d3e5803cd133393cd72c12cfe
Ivan Guzman <hello@ivanguzman.dev>
2022-05-21 00:04:51 -0700

    Remove exclamation marks

---
+++ 8-kyu/JS/remove-exclamation-marks.js
@@ -0,0 +1,20 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+Kata title: Remove exclamation marks
+
+Instructions
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+function removeExclamationMarks(str) {
+    return str.split('!').join('');
+}
+
+console.log(removeExclamationMarks('Hello World!')); //'Hello World'
+console.log(removeExclamationMarks('Hi! I! am! Bob!')); //'Hi I am Bob'
