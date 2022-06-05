2e839f4ff4590e9cebce90b2e26f35fb576116d0
Ivan Guzman <hello@ivanguzman.dev>
2022-06-04 19:17:45 -0700

    Sum without highest and lowest number solution

--- 8-kyu/JS/sum-without-highest-and-lowest-number.js
+++ 8-kyu/JS/sum-without-highest-and-lowest-number.js
@@ -26,3 +26,28 @@ Input validation
 If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 
 */
+//PREP
+// Nums, Yeah, negative nums? Yeah, non num? Yeah -> null
+// returning a sum without the lowest and highest num
+function sumArray(array) {
+    //if array is empty or null return 0
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+        //else continue to sort
+    } else {
+        //remove the highest and lowest value and sum the remaining ints
+        return array
+            .sort((a, b) => a - b)
+            .slice(1, array.length - 1)
+            .reduce((acc, curr) => acc + curr, 0);
+    }
+}
+
+console.log(sumArray(null), 0);
+console.log(sumArray([]), 0);
+console.log(sumArray([3]), 0);
+console.log(sumArray([3, 5]), 0);
+console.log(sumArray([6, 2, 1, 8, 10]), 16);
+console.log(sumArray([0, 1, 6, 10, 10]), 17);
+console.log(sumArray([-6, -20, -1, -10, -12]), -28);
+console.log(sumArray([-6, 20, -1, 10, -12]), 3);
