748ec2c05ec8a2842eb6101a52b1861054f028e7
Ivan Guzman <hello@ivanguzman.dev>
2022-08-11 19:11:44 -0700

    Thinkful - Logic Drills: Traffic light

---
+++ 8-kyu/JS/thinkful-logic-drills-traffic-light.js
@@ -0,0 +1,41 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/58649884a1659ed6cb000072
+Kata title: Thinkful - Logic Drills: Traffic light
+
+Description:
+Task
+
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+Example
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+Input validation
+
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+*/
+
+function updateLight(current) {
+  if (current === 'green') {
+    return 'yellow';
+  } else if (current === 'yellow') {
+    return 'red';
+  }
+  return 'green';
+}
+
+console.log(updateLight('green'), 'yellow');
+console.log(updateLight('yellow'), 'red');
+console.log(updateLight('red'), 'green');
