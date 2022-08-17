16b4f05106fd5f447775b9ed8085c2bd41fe4b0d
Ivan Guzman <hello@ivanguzman.dev>
2022-08-16 23:26:27 -0700

    Beginner Series #4 Cockroach

---
+++ 8-kyu/JS/beginner-series-4-cockroach.js
@@ -0,0 +1,30 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+Kata title: Beginner Series #4 Cockroach
+
+Description:
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+*/
+// getting floats
+// returning converted num to cm rounded down to the int
+// km/h -> cm/s need to divide km/h by 0.036 to get cm/s
+function cockroachSpeed(s) {
+  return Math.floor(s / 0.036);
+}
+
+console.log(cockroachSpeed(1.08), 30);
+console.log(cockroachSpeed(1.09), 30);
+console.log(cockroachSpeed(0), 0);
