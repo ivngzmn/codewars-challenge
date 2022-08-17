08a6ea6962f141b5ca54f20f5dc2a540e3908342
Ivan Guzman <hello@ivanguzman.dev>
2022-08-16 23:25:28 -0700

    Double Char

---
+++ 8-kyu/JS/double-char.js
@@ -0,0 +1,33 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/56b1f01c247c01db92000076
+Kata title: Double Char
+
+Description:
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+Examples (Input -> Output):
+
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+
+Good Luck!
+*/
+function doubleChar(str) {
+  return str
+    .split('')
+    .map((char) => char + char)
+    .join('');
+}
+
+console.log(doubleChar('abcd'), 'aabbccdd');
+console.log(doubleChar('Adidas'), 'AAddiiddaass');
+console.log(doubleChar('1337'), '11333377');
+console.log(doubleChar('illuminati'), 'iilllluummiinnaattii');
+console.log(doubleChar('123456'), '112233445566');
+console.log(doubleChar('%^&*('), '%%^^&&**((');
