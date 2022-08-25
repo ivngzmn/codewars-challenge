f363d87eb42300b3369ddd9054517d62836f3fb0
Ivan Guzman <hello@ivanguzman.dev>
2022-08-24 23:34:39 -0700

    Is the string uppercase?

---
+++ 8-kyu/JS/is-the-string-uppercase.js
@@ -0,0 +1,40 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+Kata title: Is the string uppercase?
+
+Description:
+Is the string uppercase?
+Task
+
+Create a method to see whether the string is ALL CAPS.
+Examples (input -> output)
+
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+*/
+//PREP
+//
+//
+
+String.prototype.isUpperCase = function () {
+  return this.toString() === this.toUpperCase();
+};
+
+console.log('c'.isUpperCase()); //-> //False
+console.log('C'.isUpperCase()); //-> //True
+console.log('hello I AM DONALD'.isUpperCase()); //-> //False
+console.log('HELLO I AM DONALD'.isUpperCase()); //-> //True
+console.log('ACSKLDFJSgSKLDFJSKLDFJ'.isUpperCase()); //-> //False
+console.log('ACSKLDFJSGSKLDFJSKLDFJ'.isUpperCase()); //-> //True
