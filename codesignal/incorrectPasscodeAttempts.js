92033becdef93748bdb6d7cb93e0844a9e792ece
Ivan Guzman <hello@ivanguzman.dev>
2022-08-25 22:38:29 -0700

    incorrectPasscodeAttempts

--- codesignal/incorrectPasscodeAttempts.js
+++ codesignal/incorrectPasscodeAttempts.js
@@ -1,4 +1,6 @@
 /*
+Title: incorrectPasscodeAttempts
+
 One Very Important User (VIU) has a Very Confidential Document (VCD) stored on his Dropbox account. He doesn't let anyone see the VCD, especially his roommates who often have access to his devices.
 
 Opening the Dropbox mobile app on the VIU's tablet requires a four-digit passcode. To ensure the confidentiality of the stored information, the device is locked out of Dropbox after 10 consecutive failed passcode attempts. We need to implement a function that, given an array of attempts made throughout the day and the correct passcode, checks to see if the device should be locked, i.e. 10 or more consecutive failed passcode attempts were made.
