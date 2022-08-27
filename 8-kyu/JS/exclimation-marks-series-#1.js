71622038df5c821db42e7113fe0e55b917ac5705
Ivan Guzman <hello@ivanguzman.dev>
2022-08-26 23:58:26 -0700

    Exclamation marks series #1

---
+++ 8-kyu/JS/exclimation-marks-series-#1.js
@@ -0,0 +1,34 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/57fae964d80daa229d000126
+Kata title: Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+Examples
+
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+function remove(string) {
+  //   console.log(string.split(''));
+  if (string.slice(-1) === '!') {
+    return string.slice(0, -1);
+  }
+  return string;
+}
+
+console.log(remove('Hi!'));
+console.log(remove('Hi!!!'));
+console.log(remove('!Hi'));
+console.log(remove('!Hi'));
+console.log(remove('Hi! Hi!'));
