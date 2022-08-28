5b3ca42cf0827529700b1d55d124ead9adb1b2e1
Ivan Guzman <hello@ivanguzman.dev>
2022-08-28 04:34:39 -0700

    Grasshopper - Combine strings

---
+++ 8-kyu/JS/grasshopper-combine-strings.js
@@ -0,0 +1,28 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/55f73f66d160f1f1db000059
+Kata title: Grasshopper - Combine strings
+
+Instructions
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+
+returns:
+
+'James Stevens'
+*/
+
+function combineNames(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+console.log(combineNames('James', 'Stevens'));
+console.log(combineNames('Bob', 'Burger'));
