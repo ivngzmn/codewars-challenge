3534c0a6ff457c4a93f08a5f7b1d1058ccd57756
Ivan Guzman <ivanguzmandev@gmail.com>
2023-07-05 21:06:52 -0700

    solution for reverse list order

---
+++ 8-kyu/JS/reverse-list-order.js
@@ -0,0 +1,28 @@
+/*
+░█████╗░░█████╗░██████╗░███████╗░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+kata URL: https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+Kata title: Reverse list order
+
+Description:
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+Examples (Input -> Output)
+
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+*/
+
+function reverseList(list) {
+    let reverse = list.toString().split(',').reverse().map(str => parseInt(str, 10)); // no need to turn arr of nums to string. Can just use list.reverse() and gtg.
+    
+    return reverse;
+}
+
+console.log(reverseList([3,1,5,4])), [4,5,1,3];
+console.log(reverseList([1,2,3,4])), [4,3,2,1];
