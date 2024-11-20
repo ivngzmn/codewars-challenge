90b4f75bd6ea61ec503c2aa6d087cdaecc3830cc
Ivan Guzman <ivanguzmandev@gmail.com>
2024-11-20 14:53:18 -0800

    function - squaring an argument

---
+++ 8-kyu/JS/function-2-squaring-an-argument.js
@@ -0,0 +1,13 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+P int? Y, float? N, strings? N
+R return the number squared
+E --> see below
+P
+ */
+
+function square(number) {
+    return number ** 2;
+}
+
+console.log(square(3), 9) // return 9
