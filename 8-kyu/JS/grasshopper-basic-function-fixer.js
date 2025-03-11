ae0cc22e6c289fe31bccac6da03d57dd691a7f37
Ivan Guzman <ivanguzmandev@gmail.com>
2025-03-10 23:21:36 -0700

    test and fix func

--- 8-kyu/JS/grasshopper-basic-function-fixer.js
+++ 8-kyu/JS/grasshopper-basic-function-fixer.js
@@ -7,5 +7,9 @@ Can you help me fix the function?
 
 function addFive(num) {
   var total = num + 5;
-  return num;
+  return total;
 }
+
+console.log(addFive(5), 10);
+console.log(addFive(0), 5);
+console.log(addFive(-5), 0);
