45f659daa5bbb0580376b324fe1a04b0befbc521
Ivan Guzman <ivanguzmandev@gmail.com>
2025-03-11 21:59:34 -0700

    add combat function

--- 8-kyu/JS/grasshopper-terminal-game-combat-function.js
+++ 8-kyu/JS/grasshopper-terminal-game-combat-function.js
@@ -2,3 +2,7 @@
 Description:
 Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
 */
+
+function combat(health, damage) {
+  // Write your code here
+}
